e8289a56bd86219487f6957f3e61a23afc02e12d
ghanshampatle <122206079+ghanshampatle@users.noreply.github.com>
2023-01-08 23:03:06 +0530

    File added

---
+++ src/Message.java
@@ -0,0 +1,11 @@
+// Assignment No.01
+//1. Write the Java program to print the Velocity Corporate training center message on console.
+
+
+public class Message {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Rajesh Patle");
+	}
+}
